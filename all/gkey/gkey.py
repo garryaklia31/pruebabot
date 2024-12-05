c3fe08ac7332654aef59cfddb6fc4e36668e5d0b
MisterChk <c19913565@gmail.com>
2024-12-04 22:16:14 -0500

    iniciado

---
+++ all/gkey/gkey.py
@@ -0,0 +1,26 @@
+from all.configs._def_main_ import*
+
+@retry('delkeys')
+async def delallkeys(_, message):
+    ID = message.from_user.id
+    FIRST = message.from_user.first_name
+
+    permisos = collection.find_one({"_id": message.from_user.id})
+
+    if permisos["role"] == "Owner":
+        pass
+    else:
+        return
+
+    result = collection_dos.delete_many({})
+    if result.deleted_count == 0:
+        return await message.reply("""<b><code>Admin Error's!</code>
+━━━━━━━━━━━━━━━━
+<code>No keys found in the database❗️</code></b>""", quote=True)
+
+    texto = f"""<b><code>Deleted All Key Successfully! </code>❇️
+━━━━━━━━━━━━━━━━
+<i>Keys Deleted</i> <code>{result.deleted_count}</code>
+━━━━━━━━━━━━━━━━
+<i>Deleted By</i> <a href="tg://user?id={ID}">{FIRST}</a> [<code>{permisos['role']}</code>]</b>"""
+    await message.reply(texto, quote=True)
---
+++ all/proxis/array_proxies.py
@@ -0,0 +1,13 @@
+#PROXIS LIMPIAS CAMBIAR CUANDO DEJEN DE FUNCIONAR 
+
+proxies = [
+    'http://mwjfsqbt-rotate:plxg0wskkci1@p.webshare.io:80',
+    'http://thqocqri-rotate:debn99lhwlu4@p.webshare.io:80',
+    'http://psiihqvp-rotate:ixpxh7emxmx7@p.webshare.io:80',
+    'http://zaotcems-rotate:lpytvgdtqui3@p.webshare.io:80',
+    'http://kalgpsyn-rotate:vlfvpdfsy85o@p.webshare.io:80',
+    'http://abxzkfmg-rotate:u6ds96f1s1a1@p.webshare.io:80',
+    'http://tujetyem-rotate:rh627bedlo09@p.webshare.io:80',
+    'http://ldlbqbpj-rotate:lreqnm214sev@p.webshare.io:80',
+    'http://rmwmzodn-rotate:dsn7b6e6aveg@p.webshare.io:80',
+]
